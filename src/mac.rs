469b0fc5672f1e3fbecbc0edfbe5d1cf45b70f3e
Sotaro Tommykawa <tommysouta@gmail.com>
2021-01-20 20:49:57 +0900

    fix mac

--- src/mac.rs
+++ src/mac.rs
@@ -1,4 +1,4 @@
-use core_text::{font_collection::{create_for_all_families, get_family_names}, font_manager::CTFontManagerCopyAvailableFontURLs};
+use core_text::font_collection::create_for_all_families;
 
 pub struct Sys;
 impl Sys {
@@ -8,7 +8,7 @@ impl Sys {
                 .map(|descriptor| descriptor.font_path())
                 .filter(|path| path.is_some())
                 .map(|path| path?.to_str())
-                .for_each(|path| println!("{}", path));
+                .for_each(|path| println!("{}", path.unwrap()));
         }
     }
 }
